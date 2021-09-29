9995fda173dd736a2a5b8ecd8cf745b5045b1fab
Elian Bollue <elian.bollue@student.ucll.be>
2021-09-29 15:23:05 +0200

    Update Movie.java

--- 1-Refactoring/src/domain/Movie.java
+++ 1-Refactoring/src/domain/Movie.java
@@ -1,4 +1,15 @@
 package domain;
 
 public class Movie extends Product {
+    String title,id;
+
+    public Movie(String title, String id){
+        super(title,id,"M");
+        this.title = title;
+        this.id = id;
+    }
+
+    public double getPrice(int days){
+        return days * 3;
+    }
 }
