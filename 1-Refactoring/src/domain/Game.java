49a520d87ee7bee4636c86adef6763a7fcb81886
Elian Bollue <elian.bollue@student.ucll.be>
2021-09-29 15:29:14 +0200

    Update Game.java

--- 1-Refactoring/src/domain/Game.java
+++ 1-Refactoring/src/domain/Game.java
@@ -1,4 +1,11 @@
 package domain;
 
 public class Game extends Product {
+    public Game(String title,String id){
+        super(title,id,"G");
+    }
+
+    public double getPrice(int days){
+        return days * 3;
+    }
 }
